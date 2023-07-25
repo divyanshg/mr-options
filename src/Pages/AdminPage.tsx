728965443d2d62258188420328b5c877210ace76
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-25 04:43:37 +0000

    test

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -50,7 +50,14 @@ const SurveyTable = ({ data }: {data: any}) => {
 
 
 export default function AdminPage(): JSX.Element{
-    const [allData, setAllData] = useState([])
+    const [allData, setAllData] = useState<any[]>([])
+    const [selectedRollNumber, setSelectedRollNumber] = useState(allData[0].RollNumber);
+
+    const handleSelectChange = (event: any) => {
+      setSelectedRollNumber(event.target.value);
+    };
+  
+    const selectedStudentData = allData.find((student: any) => student.RollNumber === selectedRollNumber);
 
     useEffect(() => {
         fetch("https://script.googleusercontent.com/macros/echo?user_content_key=tVwz64Sl1klQon_lZLW4myQqx3cYSxOkcYVyT8pZG_u7bMb01fgxD5IqM4yADIdduFCn7nN3SEi38uOiVm1GxqSppL_xDREzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMi42bRYzZy3riDdvlAkWszRm9m-R0scv3xLo0-S3yCaacQCdOZWlgNkRUrXQEBQUV1gSvSZXOOM4ZYbj1USgFmmKbFgBBuMm9z9Jw9Md8uu&lib=Mj9sCKg167WlSD7i4YbtxJMb_W9z17S8P")
@@ -63,6 +70,23 @@ export default function AdminPage(): JSX.Element{
     }, [])
 
     return (
-        <SurveyTable data={allData}/>
+       <div>
+      <h1>Survey Results</h1>
+      <div>
+        <label htmlFor="selectRollNumber">Select RollNumber:</label>
+        <select
+          id="selectRollNumber"
+          value={selectedRollNumber}
+          onChange={handleSelectChange}
+        >
+          {allData.map((student: any) => (
+            <option key={student.RollNumber} value={student.RollNumber}>
+              {student.RollNumber}
+            </option>
+          ))}
+        </select>
+      </div>
+      {selectedStudentData && <SurveyTable data={[selectedStudentData]} />}
+    </div>
     )
 }
