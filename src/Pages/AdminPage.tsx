a22d8cbb4286df45001721199020464c61400ab9
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-25 04:59:48 +0000

    test

--- src/Pages/AdminPage.tsx
+++ src/Pages/AdminPage.tsx
@@ -93,6 +93,7 @@ export default function AdminPage(): JSX.Element{
     const selectedStudentData = allData.find((student: any) => student.RollNumber === selectedRollNumber);
 
     useEffect(() => {
+        if(!loggedIn) return
         fetch("https://script.googleusercontent.com/macros/echo?user_content_key=tVwz64Sl1klQon_lZLW4myQqx3cYSxOkcYVyT8pZG_u7bMb01fgxD5IqM4yADIdduFCn7nN3SEi38uOiVm1GxqSppL_xDREzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMi42bRYzZy3riDdvlAkWszRm9m-R0scv3xLo0-S3yCaacQCdOZWlgNkRUrXQEBQUV1gSvSZXOOM4ZYbj1USgFmmKbFgBBuMm9z9Jw9Md8uu&lib=Mj9sCKg167WlSD7i4YbtxJMb_W9z17S8P")
             .then(res => res.json())
             .then(res => {
@@ -103,7 +104,7 @@ export default function AdminPage(): JSX.Element{
                 alert("Something went wrong")
                 console.log(err)
             })
-    }, [])
+    }, [loggedIn])
 
     if(loggedIn){
 
