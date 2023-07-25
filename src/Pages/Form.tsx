95f44ea596e898d77401d13deaf37bc6c2e3cdc9
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-25 08:56:52 +0000

    test

--- src/Pages/Form.tsx
+++ src/Pages/Form.tsx
@@ -56,20 +56,16 @@ const FormPage = () => {
       .catch((error) => console.error("Error fetching questions:", error));
   }, []);
 
-  const getNewData = (roll: number) => {
-    if(!isSubmitted) return
-    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=tVwz64Sl1klQon_lZLW4myQqx3cYSxOkcYVyT8pZG_u7bMb01fgxD5IqM4yADIdduFCn7nN3SEi38uOiVm1GxqSppL_xDREzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMi42bRYzZy3riDdvlAkWszRm9m-R0scv3xLo0-S3yCaacQCdOZWlgNkRUrXQEBQUV1gSvSZXOOM4ZYbj1USgFmmKbFgBBuMm9z9Jw9Md8uu&lib=Mj9sCKg167WlSD7i4YbtxJMb_W9z17S8P")
-        .then(res => res.json())
-        .then(res => {
-            setIsLoading(false)
-            console.log({res})
-            console.log(res.filter((r:any) => r.RollNumber === roll))
-        })
-        .catch(err => {
-            alert("Something went wrong")
-            console.log(err)
-        })
-}
+  const getNewData = (arr: { questionId: string; optionIndex: string }[]) => {
+    const obj: Record<string, number> = {}; // Explicitly define the type of obj
+  
+    arr.forEach((item: { questionId: string; optionIndex: string }) => {
+      obj[`Question_${item.questionId}`] = parseInt(item.optionIndex, 10);
+    });
+  
+    return obj;
+  };
+  
 
   const formatResponses = (
     responses: Record<string, string | number>
@@ -150,7 +146,7 @@ const FormPage = () => {
         } else {
           alert("Form submitted successfully");
           setIsSubmitted(true)
-          getNewData(data.RollNumber)
+          console.log(getNewData(data.Responses))
         }
       })
       .catch((error) => {
