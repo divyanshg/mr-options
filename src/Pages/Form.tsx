e5e7e08f9fe2fa1a473e81b5bf7f7a008de710ea
Divyansh Gupta <divyanshg809@gmail.com>
2023-07-25 08:47:08 +0000

    test

--- src/Pages/Form.tsx
+++ src/Pages/Form.tsx
@@ -41,7 +41,7 @@ const FormPage = () => {
     useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
-  const [allData, setAllData] = useState<any[]>([])
+  //const [allData, setAllData] = useState<any[]>([])
 
   useEffect(() => {
     // Fetch questions from Sanity
@@ -56,19 +56,19 @@ const FormPage = () => {
       .catch((error) => console.error("Error fetching questions:", error));
   }, []);
 
-  useEffect(() => {
+  const getNewData = (roll: number) => {
     if(!isSubmitted) return
     fetch("https://script.googleusercontent.com/macros/echo?user_content_key=tVwz64Sl1klQon_lZLW4myQqx3cYSxOkcYVyT8pZG_u7bMb01fgxD5IqM4yADIdduFCn7nN3SEi38uOiVm1GxqSppL_xDREzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMi42bRYzZy3riDdvlAkWszRm9m-R0scv3xLo0-S3yCaacQCdOZWlgNkRUrXQEBQUV1gSvSZXOOM4ZYbj1USgFmmKbFgBBuMm9z9Jw9Md8uu&lib=Mj9sCKg167WlSD7i4YbtxJMb_W9z17S8P")
         .then(res => res.json())
         .then(res => {
             setIsLoading(false)
-            console.log(res.filter((r:any) => r.RollNumber === initialValues.RollNumber))
+            console.log(res.filter((r:any) => r.RollNumber === roll))
         })
         .catch(err => {
             alert("Something went wrong")
             console.log(err)
         })
-}, [isSubmitted])
+}
 
   const formatResponses = (
     responses: Record<string, string | number>
@@ -149,6 +149,7 @@ const FormPage = () => {
         } else {
           alert("Form submitted successfully");
           setIsSubmitted(true)
+          getNewData(data.RollNumber)
         }
       })
       .catch((error) => {
